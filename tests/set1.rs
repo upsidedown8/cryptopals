24d631f6adbb14b262817c8b590ab17873d52190
Tom Thorogood <tomthorogood@outlook.com>
2021-07-15 03:37:36 +0100

    Completed set 1 challenge 8

--- tests/set1.rs
+++ tests/set1.rs
@@ -130,3 +130,36 @@ fn challenge7() {
 
     assert_eq!(expected_b64, base64::encode(&dec));
 }
+
+#[test]
+fn challenge8() {
+    use std::collections::HashSet;
+    use std::fs::File;
+    use std::io::BufRead;
+
+    let filename = "data/set1/challenge8.txt";
+    let file = File::open(filename).unwrap();
+
+    let mut ecb_line = String::new();
+
+    'outer: for line in std::io::BufReader::new(file).lines() {
+        let line = line.unwrap();
+        let mut set = HashSet::new();
+
+        let num_blocks = line.len() / (16 * 2);
+        for i in 0..num_blocks {
+            let block = String::from(&line[i * 32..(i + 1) * 32]);
+
+            if set.contains(&block) {
+                ecb_line = line;
+                break 'outer;
+            } else {
+                set.insert(block);
+            }
+        }
+    }
+
+    let expected_hex = "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a";
+
+    assert_eq!(expected_hex, ecb_line);
+}
